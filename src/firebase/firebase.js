afad99c619d077ae9eb478abea4d91fbfd731f30
Md Anas Sabah <mdanassabah@gmail.com>
2023-03-13 20:41:22 +0530

    update: integrated firebase

---
+++ src/firebase/firebase.js
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDTYFG_1VG37ypRpaBcVS772YRFJasrA9Q",
+  authDomain: "cine-stream.firebaseapp.com",
+  projectId: "cine-stream",
+  storageBucket: "cine-stream.appspot.com",
+  messagingSenderId: "342047269654",
+  appId: "1:342047269654:web:551f4f8d4d2080614b6637",
+  measurementId: "G-6HTF74DP69",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export default app;
